de9836085f58471d5375089b1f5c2a4f8ef276f1
neko3cs <minaton.0722.net@gmail.com>
2024-01-17 20:18:22 +0900

    feature: glutからglfwに変更
    glutは古くてglfwの方が実装が楽らしい

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,44 @@
 #include "FifteenPuzzle.h"
 #include <iostream>
-#include <GLUT/glut.h>
+#include <GLFW/glfw3.h>
+
+void handleKeyboard(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+  if (action == GLFW_PRESS || action == GLFW_REPEAT)
+  {
+    switch (key)
+    {
+    case GLFW_KEY_Q:
+      std::cout << "Exit" << std::endl;
+      glfwSetWindowShouldClose(window, GLFW_TRUE);
+      break;
+    case GLFW_KEY_LEFT:
+      std::cout << "left key input!" << std::endl;
+      break;
+    case GLFW_KEY_RIGHT:
+      std::cout << "right key input!" << std::endl;
+      break;
+    case GLFW_KEY_UP:
+      std::cout << "up key input!" << std::endl;
+      break;
+    case GLFW_KEY_DOWN:
+      std::cout << "down key input!" << std::endl;
+      break;
+    default:
+      std::cout << "Invalid character was inputted: " << glfwGetKeyName(key, scancode) << std::endl;
+      break;
+    }
+  }
+}
+
+void reshape(GLFWwindow *window, int width, int height)
+{
+  glViewport(0, 0, width, height);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(0, 6, 0, 6, -1, 1);
+  glMatrixMode(GL_MODELVIEW);
+}
 
 void drawGrid()
 {
@@ -22,83 +60,45 @@ void drawGrid()
   }
 }
 
-void display()
+int main()
 {
-  glClear(GL_COLOR_BUFFER_BIT);
-  drawGrid();
-  glFlush();
-}
+  if (!glfwInit())
+  {
+    std::cerr << "Failed to initialize GLFW" << std::endl;
+    return -1;
+  }
 
-void reshape(int width, int height)
-{
-  glViewport(0, 0, width, height);
+  GLFWwindow *window = glfwCreateWindow(640, 480, "15 Puzzle", nullptr, nullptr);
+  if (!window)
+  {
+    std::cerr << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+  glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  gluOrtho2D(0, 6, 0, 6);
+  glOrtho(0, 6, 0, 6, -1, 1);
   glMatrixMode(GL_MODELVIEW);
-}
 
-void handleKeyboard(unsigned char key, int x, int y)
-{
-  switch (key)
+  glfwSetKeyCallback(window, handleKeyboard);
+  glfwSetWindowSizeCallback(window, reshape);
+  glfwSetInputMode(window, GLFW_STICKY_KEYS, GLFW_TRUE);
+
+  while (!glfwWindowShouldClose(window))
   {
-  case 'q':
-    std::cout << "Exit" << std::endl;
-    std::exit(EXIT_SUCCESS);
-    break;
-  default:
-    std::cout << "Invalid character was inputted: " << key << std::endl;
-    break;
+    glClear(GL_COLOR_BUFFER_BIT);
+    drawGrid();
+
+    // FifteenPuzzle fifteenPuzzle;
+    // fifteenPuzzle.Run();
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
   }
-}
 
-void handleSpecialKey(int key, int x, int y)
-{
-  switch (key)
-  {
-  case GLUT_KEY_LEFT:
-    std::cout << "left key input!" << std::endl;
-    break;
-  case GLUT_KEY_RIGHT:
-    std::cout << "right key input!" << std::endl;
-    break;
-  case GLUT_KEY_UP:
-    std::cout << "up key input!" << std::endl;
-    break;
-  case GLUT_KEY_DOWN:
-    std::cout << "down key input!" << std::endl;
-    break;
-  default:
-    break;
-  }
-  glutPostRedisplay();
-}
-
-void initGL()
-{
-  const auto windowWidth = 640;
-  const auto windowHeight = 480;
-  glutInitDisplayMode(GLUT_RGB);
-  glutInitWindowSize(windowWidth, windowHeight);
-  glutInitWindowPosition(
-      (glutGet(GLUT_SCREEN_WIDTH) - windowWidth) / 2,
-      (glutGet(GLUT_SCREEN_HEIGHT) - windowHeight) / 2);
-  glutCreateWindow("15 Puzzle");
-  glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-  glutDisplayFunc(display);
-  glutReshapeFunc(reshape);
-  glutKeyboardFunc(handleKeyboard);
-  glutSpecialFunc(handleSpecialKey);
-}
-
-int main(int argc, char *argv[])
-{
-  glutInit(&argc, argv);
-  initGL();
-  glutMainLoop();
-
-  FifteenPuzzle fifteenPuzzle;
-  fifteenPuzzle.Run();
-
+  glfwTerminate();
   return 0;
 }
