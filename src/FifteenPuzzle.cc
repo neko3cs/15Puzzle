1b3a71f051373ee096e8aae7b88d3d992f6e0a4b
neko3cs <minaton.0722.net@gmail.com>
2024-01-18 15:25:22 +0900

    feature: ひとまずパズルクラスに移植

--- src/FifteenPuzzle.cc
+++ src/FifteenPuzzle.cc
@@ -1,53 +1,118 @@
 #include "FifteenPuzzle.h"
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 FifteenPuzzle::FifteenPuzzle()
 {
   board.Initialize();
 }
 
-void FifteenPuzzle::Run()
+void FifteenPuzzle::handleKeyboard(GLFWwindow *window, int key, int scancode, int action, int mods)
 {
-  while (true)
+  if (action == GLFW_PRESS || action == GLFW_REPEAT)
   {
-    board.Show();
-
-    char input;
-    std::cout << "Input direction(H: Left, J: Up, K: Down, L: Right) or quit(q, quit): " << std::flush;
-    std::cin >> input;
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-    if (input == 'q')
+    switch (key)
     {
+    case GLFW_KEY_Q:
       std::cout << "Exit" << std::endl;
+      glfwSetWindowShouldClose(window, GLFW_TRUE);
+      break;
+    case GLFW_KEY_LEFT:
+      std::cout << "left key input!" << std::endl;
+      // board.MovePanel(MoveDirection::Left);  // FIXME: staticメンバ関数だからインスタンスのメンバ変数を呼べない
+      break;
+    case GLFW_KEY_RIGHT:
+      std::cout << "right key input!" << std::endl;
+      // board.MovePanel(MoveDirection::Right);  // FIXME: staticメンバ関数だからインスタンスのメンバ変数を呼べない
+      break;
+    case GLFW_KEY_UP:
+      std::cout << "up key input!" << std::endl;
+      // board.MovePanel(MoveDirection::Up);  // FIXME: staticメンバ関数だからインスタンスのメンバ変数を呼べない
+      break;
+    case GLFW_KEY_DOWN:
+      std::cout << "down key input!" << std::endl;
+      // board.MovePanel(MoveDirection::Down);  // FIXME: staticメンバ関数だからインスタンスのメンバ変数を呼べない
+      break;
+    default:
+      std::cout << "Invalid character was inputted: " << glfwGetKeyName(key, scancode) << std::endl;
       break;
     }
-    else if (input == 'h' || input == 'H')
-    {
-      board.MovePanel(MoveDirection::Left);
-    }
-    else if (input == 'l' || input == 'L')
-    {
-      board.MovePanel(MoveDirection::Right);
-    }
-    else if (input == 'j' || input == 'J')
-    {
-      board.MovePanel(MoveDirection::Up);
-    }
-    else if (input == 'k' || input == 'K')
-    {
-      board.MovePanel(MoveDirection::Down);
-    }
-    else
-    {
-      std::cout << "Invalid character was inputted: " << input << std::endl;
-    }
+  }
+}
+
+void FifteenPuzzle::reshape(GLFWwindow *window, int width, int height)
+{
+  glViewport(0, 0, width, height);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(0, 6, 0, 6, -1, 1);
+  glMatrixMode(GL_MODELVIEW);
+}
+
+void FifteenPuzzle::drawGrid()
+{
+  glColor3f(0.0f, 0.0f, 0.0f);
+
+  for (int i = 1; i < 6; i++)
+  {
+    glBegin(GL_LINES);
+    glVertex2f(i, 1);
+    glVertex2f(i, 5);
+    glEnd();
+  }
+  for (int i = 1; i < 6; i++)
+  {
+    glBegin(GL_LINES);
+    glVertex2f(1, i);
+    glVertex2f(5, i);
+    glEnd();
+  }
+}
+
+void FifteenPuzzle::Run()
+{
+  if (!glfwInit())
+  {
+    std::cerr << "Failed to initialize GLFW" << std::endl;
+    return;
+  }
+
+  GLFWwindow *window = glfwCreateWindow(640, 480, "15 Puzzle", nullptr, nullptr);
+  if (!window)
+  {
+    std::cerr << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return;
+  }
+
+  glfwMakeContextCurrent(window);
+  glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(0, 6, 0, 6, -1, 1);
+  glMatrixMode(GL_MODELVIEW);
+
+  glfwSetKeyCallback(window, handleKeyboard);
+  glfwSetWindowSizeCallback(window, reshape);
+  glfwSetInputMode(window, GLFW_STICKY_KEYS, GLFW_TRUE);
+
+  while (!glfwWindowShouldClose(window))
+  {
+    glClear(GL_COLOR_BUFFER_BIT);
+    drawGrid();
+
+    // board.Show(); // FIXME: GUIで実装する
 
     if (board.IsSolved())
     {
       // パズルが解けたら褒めて終了
       std::cout << "Congratulations! You solved puzzle!" << std::endl;
-      break;
+      glfwSetWindowShouldClose(window, GLFW_TRUE);
     }
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
   }
+
+  glfwTerminate();
 }
--- src/FifteenPuzzle.h
+++ src/FifteenPuzzle.h
@@ -1,10 +1,14 @@
 #pragma once
 #include "Board.h"
+#include <GLFW/glfw3.h>
 
 class FifteenPuzzle
 {
 private:
 	Board board;
+	static void handleKeyboard(GLFWwindow *window, int key, int scancode, int action, int mods);
+	static void reshape(GLFWwindow *window, int width, int height);
+	static void drawGrid();
 
 public:
 	FifteenPuzzle();
