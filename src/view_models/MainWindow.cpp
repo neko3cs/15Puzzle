d883b74ac97a93f6c6cf951c2ff6a35e4f60b22d
neko3cs <minaton.0722.net@gmail.com>
2024-02-14 17:12:51 +0900

    feature: MainWindowのvectorからGrid生成

--- src/view_models/MainWindow.cpp
+++ src/view_models/MainWindow.cpp
@@ -1,4 +1,5 @@
 #include <QDebug>
+#include <algorithm>
 #include "MainWindow.hpp"
 
 MainWindow::MainWindow()
@@ -43,5 +44,41 @@ void MainWindow::SetText(const QString &text)
   {
     _text = text;
     emit TextChanged();
+  }
+}
+
+QVariantList MainWindow::GetDataList() const
+{
+  QVariantList list;
+  for (auto value : _dataList)
+  {
+    list.append(QVariant::fromValue(value));
+  }
+
+  return list;
+}
+
+void MainWindow::SetDataList(const QVariantList &list)
+{
+  std::vector<int> convedList;
+  for (const QVariant &item : list)
+  {
+    if (item.canConvert<int>())
+    {
+      convedList.push_back(item.value<int>());
+    }
+    else
+    {
+      qWarning();
+    }
+  }
+
+  bool listIsSame = _dataList.size() == convedList.size() &&
+                    std::equal(_dataList.begin(), _dataList.end(), convedList.begin(), convedList.end());
+
+  if (listIsSame)
+  {
+    _dataList = convedList;
+    emit DataListChanged();
   }
 }
--- src/view_models/MainWindow.hpp
+++ src/view_models/MainWindow.hpp
@@ -1,12 +1,15 @@
 #pragma once
 #include <QObject>
+#include <QVariant>
 #include <QString>
+#include <vector>
 #include "../models/Board.hpp"
 
 class MainWindow : public QObject
 {
   Q_OBJECT
   Q_PROPERTY(QString Text READ GetText WRITE SetText NOTIFY TextChanged)
+  Q_PROPERTY(QVariantList DataList READ GetDataList WRITE SetDataList NOTIFY DataListChanged)
 
 public:
   MainWindow();
@@ -19,11 +22,15 @@ public:
 
   QString GetText() const;
   void SetText(const QString &text);
+  QVariantList GetDataList() const;
+  void SetDataList(const QVariantList &list);
 
 signals:
   void TextChanged();
+  void DataListChanged();
 
 private:
   Board board;
   QString _text;
+  std::vector<int> _dataList = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 };
